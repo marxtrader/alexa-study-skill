f8a4bc249e34913ee9c61a6d6d43014b9e88ba46
MARX <admin@marx.tech>
2019-01-01 18:35:03 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,19 +1,15 @@
 
 var config = {
 	database: {
-			host:      '54.148.123.122',     // database host
-			user:       'twilio',         // your database username
-			password: '1qazXSW@',         // your database password
+			host:      '',     // database host
+			user:       '',         // your database username
+			password: '',         // your database password
 			port:       3306,         // default MySQL port
 			db:       'webdev'         // your database name
 	},
 	server: {
 			host: '127.0.0.1',
 			port: '3000'
-	},
-	twilio : {
-			authToken : "cf7697eef088f4898b4f66da704224c0",
-			accountSid : "AC1bc748cb6a75272642d6f9f70d27793f"
 	},
 };
 
