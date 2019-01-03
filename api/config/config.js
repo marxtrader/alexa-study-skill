ca1a57609a23c76cc5da0a30afe8f0fc31f2c758
Chris Ranni <admin@marx.tech>
2019-01-02 20:28:03 -0500

    added api config

---
+++ api/config/config.js
@@ -0,0 +1,16 @@
+
+var config = {
+	database: {
+			host:      'webdev.marxcloud.com',     // database host
+			user:       'twilio',         // your database username
+			password: '1qazXSW@',         // your database password
+			port:       3306,         // default MySQL port
+			db:       'webdev'         // your database name
+	},
+	server: {
+			host: '127.0.0.1',
+			port: '4000'
+	},
+};
+
+module.exports = config
